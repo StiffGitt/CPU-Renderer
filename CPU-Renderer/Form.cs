a4e88298d3b9a7e4c8ab8f7dbcb042681e7ed0c8
StiffGitt <131384247+StiffGitt@users.noreply.github.com>
2024-02-04 10:48:48 +0100

    Add project files.

---
+++ CPU-Renderer/Form.cs
@@ -0,0 +1,10 @@
+namespace CPU_Renderer
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
